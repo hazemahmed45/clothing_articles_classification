1cabff13dc88ea07d9b9181350ca48077cd86439
hazemahmed45 <hazemahmed45@gmail.com>
2021-11-19 00:27:43 +0200

    minor changes

--- model.py
+++ model.py
@@ -66,11 +66,3 @@ class FashionMnistClassifier(nn.Module):
         # print(x.shape)
         x=self.classifier(x)
         return x
-# model=ClothClassifier(5).cuda()
-# input = torch.randn(1, 3, 224, 224).cuda()
-# print(summary(model,(3,224,224)))
-# macs, params = profile(model, inputs=(input, ))
-# macs_params=clever_format([macs, params], "%.3f")
-# # print(macs,params)
-# model=FashionMnistClassifier(10)
-# print(model(torch.randn((1,1,28,28))).shape)
