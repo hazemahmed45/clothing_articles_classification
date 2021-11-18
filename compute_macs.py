db22c9f65b5085ef8130e6f6d130e55474537701
hazemahmed45 <hazemahmed45@gmail.com>
2021-11-19 00:27:22 +0200

    finished with compute_macs

--- compute_macs.py
+++ compute_macs.py
@@ -6,15 +6,15 @@ from thop import profile,clever_format
 
 
 
-model=ClothClassifier(5).cuda()
+model=ClothClassifier(20).cuda()
 input = torch.randn(1, 3, 224, 224).cuda()
-print(summary(model,(3,224,224)))
+# print(summary(model,(3,224,224)))
 macs, params = profile(model, inputs=(input, ))
 macs_params=clever_format([macs, params], "%.3f")
 print(str(macs/1e9),"G",str(params/1e6),"M")
 
 input=torch.randn((1,3,28,28)).cuda()
 model=FashionMnistClassifier(10).cuda()
-print(summary(model,(3,28,28)))
+# print(summary(model,(3,28,28)))
 macs,params=profile(model,inputs=(input,))
 print(str(macs/1e9),"G",str(params/1e6),"M")
