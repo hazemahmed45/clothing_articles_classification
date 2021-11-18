cb739a07dfaf9675bc9dd9f49489169481adeb52
hazemahmed45 <hazemahmed45@gmail.com>
2021-11-19 00:30:37 +0200

    seperate train script for each dataset

--- train_clothing_dataset.py
+++ train_clothing_dataset.py
@@ -8,7 +8,7 @@ from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
 from augmentation import get_transform_pipeline
-from metric import Accuracy,RunningLoss
+from metric import Accuracy,RunningLoss,Precision,Recall,F1Score
 from callbacks import CheckpointCallback
 from torchsummary import summary
 import os
@@ -19,7 +19,7 @@ from torchvision.transforms import transforms
 
 
 
-EXP_NUM=11
+EXP_NUM=5
 CKPT_DIR='checkpoints'
 MODEL_NAME='cloth_classifier_resnet18_'+str(EXP_NUM)+'.pt'
 IMG_DIR='Dataset/KaggleClothing/images_compressed'
@@ -35,7 +35,7 @@ SHUFFLE=True
 NUM_WORKERS=8
 PIN_MEMORY=True
 EPOCHS=30
-LR=1e-4
+LR=3e-3
 TRAIN_SPLIT=0.7
 device='cuda' if torch.cuda.is_available() else 'cpu'
 config={
@@ -58,28 +58,11 @@ config={
 }
 wandb.init(name='cloth_classification_resnet18_'+str(EXP_NUM),config=config,job_type='classification',project="clothing-articles-classification", entity="hazem45")
 transform_dict={
-    'train':get_transform_pipeline(IMG_WIDTH,IMG_HEIGHT),
+    'train':get_transform_pipeline(IMG_WIDTH,IMG_HEIGHT,False),
     'valid':get_transform_pipeline(IMG_WIDTH,IMG_HEIGHT,False)
 }
 dataset_splitter=ClothDatasetSplitter(img_dir=IMG_DIR,meta_csv=IMG_META_DIR,transforms_dict=transform_dict)
 train_dataset,valid_dataset=next(iter(dataset_splitter.generate_train_valid_dataset(TRAIN_SPLIT)))
-train_transform=transforms.Compose([
-    transforms.Resize((IMG_WIDTH,IMG_HEIGHT)),
-    transforms.GaussianBlur(3),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomPerspective(),
-    transforms.RandomInvert(),
-    transforms.RandomRotation(15),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-])
-valid_transform=transforms.Compose([
-    transforms.Resize((IMG_WIDTH,IMG_HEIGHT)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-])
-# train_dataset=FashionMnist(TRAIN_META,train_transform)
-# valid_dataset=FashionMnist(VALID_META,valid_transform)
 
 train_loader=DataLoader(dataset=train_dataset,batch_size=BATCH_SIZE,shuffle=SHUFFLE,num_workers=NUM_WORKERS,pin_memory=PIN_MEMORY)
 valid_loader=DataLoader(dataset=valid_dataset,batch_size=BATCH_SIZE,shuffle=SHUFFLE,num_workers=NUM_WORKERS,pin_memory=PIN_MEMORY)
@@ -90,11 +73,14 @@ model.to(device)
 
 print(summary(model,input_data=(3,IMG_WIDTH,IMG_HEIGHT)))
 
-
-criterion=CrossEntropyLoss()
+classes_count=train_dataset.get_classes_count()
+criterion=CrossEntropyLoss(weight=torch.tensor(1-(classes_count/classes_count.sum()),dtype=torch.float32).cuda())
 optimizer=Adam(params=model.parameters(),lr=LR)
 # schedular=StepLR(optimizer=optimizer,step_size=5)
-metric=Accuracy()
+acc_metric=Accuracy()
+prec_metric=Precision()
+recall_metric=Recall()
+f1_metric=F1Score()
 running_loss=RunningLoss()
 ckpt_callback=CheckpointCallback(os.path.join(CKPT_DIR,MODEL_NAME),'max',verbose=1)
 
@@ -102,8 +88,11 @@ wandb.watch(model,criterion=criterion,log_freq=1,log_graph=True)
 
 for e in range(EPOCHS):
     model.train()
-    metric.reset()
+    acc_metric.reset()
     running_loss.reset()
+    prec_metric.reset()
+    recall_metric.reset()
+    f1_metric.reset()
     log_dict={}
     iter_loop=tqdm(enumerate(train_loader),total=len(train_loader))
     # running_loss=0
@@ -118,15 +107,30 @@ for e in range(EPOCHS):
         optimizer.step()
         
         # running_loss+=loss.detach().cpu().item()
-        running_loss.update(loss)
-        metric.update(label_batch,output)
+        running_loss.update(batch_loss=loss)
+        acc_metric.update(y_true=label_batch,y_pred=output)
+        prec_metric.update(y_true=label_batch,y_pred=output)
+        recall_metric.update(y_true=label_batch,y_pred=output)
+        f1_metric.update(y_true=label_batch,y_pred=output)
         iter_loop.set_description('TRAIN LOOP E: '+str(e))
-        iter_loop.set_postfix({'LOSS':running_loss.get_value(),"ACC":metric.get_value()})
+        iter_loop.set_postfix({
+            running_loss.name:running_loss.get_value(),
+            acc_metric.name:acc_metric.get_value(),
+            prec_metric.name:prec_metric.get_value(),
+            recall_metric.name:recall_metric.get_value(),
+            f1_metric.name:f1_metric.get_value()
+            })
     log_dict['loss/train']=running_loss.get_value()
-    log_dict['accuracy/train']=metric.get_value()
+    log_dict['accuracy/train']=acc_metric.get_value()
+    log_dict['precision/train']=prec_metric.get_value()
+    log_dict['recall/train']=recall_metric.get_value()
+    log_dict['f1score/train']=f1_metric.get_value()
     model.eval()
-    metric.reset()
+    acc_metric.reset()
     running_loss.reset()
+    prec_metric.reset()
+    recall_metric.reset()
+    f1_metric.reset()
     with torch.no_grad():
         iter_loop=tqdm(enumerate(valid_loader),total=len(valid_loader))
         for ii,(img_batch,label_batch) in iter_loop:
@@ -137,15 +141,27 @@ for e in range(EPOCHS):
 
             
             # loss+=loss.detach().cpu().numpy()
-            running_loss.update(loss)
-            metric.update(label_batch,output)
+            running_loss.update(batch_loss=loss)
+            acc_metric.update(y_true=label_batch,y_pred=output)
+            prec_metric.update(y_true=label_batch,y_pred=output)
+            recall_metric.update(y_true=label_batch,y_pred=output)
+            f1_metric.update(y_true=label_batch,y_pred=output)
             iter_loop.set_description('VALID LOOP E: '+str(e))
-            iter_loop.set_postfix({'LOSS':running_loss.get_value(),"ACC":metric.get_value()})
-        ckpt_callback.check_and_save(model,metric.get_value())   
+            iter_loop.set_postfix({
+                running_loss.name:running_loss.get_value(),
+                acc_metric.name:acc_metric.get_value(),
+                prec_metric.name:prec_metric.get_value(),
+                recall_metric.name:recall_metric.get_value(),
+                f1_metric.name:f1_metric.get_value()
+            })
+        ckpt_callback.check_and_save(model,acc_metric.get_value())   
     # schedular.step()
                  
     log_dict['loss/valid']=running_loss.get_value()
-    log_dict['accuracy/valid']=metric.get_value()
+    log_dict['accuracy/valid']=acc_metric.get_value()
+    log_dict['precision/valid']=prec_metric.get_value()
+    log_dict['recall/valid']=recall_metric.get_value()
+    log_dict['f1score/valid']=f1_metric.get_value()
     log_dict['epochs']=e
     # log_dict['lr']=schedular.get_last_lr()[-1]
     wandb.log(log_dict)
--- train_fashion_mnist.py
+++ train_fashion_mnist.py
@@ -8,7 +8,7 @@ from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
 from augmentation import get_transform_pipeline
-from metric import Accuracy,RunningLoss
+from metric import Accuracy,RunningLoss,Precision,Recall,F1Score
 from callbacks import CheckpointCallback
 from torchsummary import summary
 import os
@@ -19,22 +19,21 @@ from torchvision.transforms import transforms
 
 
 
-EXP_NUM=11
+EXP_NUM=4
 CKPT_DIR='checkpoints'
-MODEL_NAME='cloth_classifier_resnet18_'+str(EXP_NUM)+'.pt'
+MODEL_NAME='fashion_mnist_'+str(EXP_NUM)+'.pt'
 
 TRAIN_META='Dataset/FashionMnist/fashion-mnist_train.csv'
 VALID_META='Dataset/FashionMnist/fashion-mnist_test.csv'
 IMG_WIDTH=28
 IMG_HEIGHT=28
 
-BATCH_SIZE=64
+BATCH_SIZE=128
 SHUFFLE=True
 NUM_WORKERS=8
 PIN_MEMORY=True
 EPOCHS=30
-LR=1e-4
-TRAIN_SPLIT=0.7
+LR=3e-3
 device='cuda' if torch.cuda.is_available() else 'cpu'
 config={
     'experiment_number':EXP_NUM,
@@ -50,9 +49,8 @@ config={
     'device':device,
     'epochs':EPOCHS,
     'learning_rate':LR,
-    'train_split':TRAIN_SPLIT
 }
-wandb.init(name='cloth_classification_resnet18_'+str(EXP_NUM),config=config,job_type='classification',project="clothing-articles-classification", entity="hazem45")
+wandb.init(name='fashion_mnist_classification_'+str(EXP_NUM),config=config,job_type='classification',project="clothing-articles-classification", entity="hazem45")
 transform_dict={
     'train':get_transform_pipeline(IMG_WIDTH,IMG_HEIGHT),
     'valid':get_transform_pipeline(IMG_WIDTH,IMG_HEIGHT,False)
@@ -60,11 +58,11 @@ transform_dict={
 
 train_transform=transforms.Compose([
     transforms.Resize((IMG_WIDTH,IMG_HEIGHT)),
-    transforms.GaussianBlur(3),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomPerspective(),
-    transforms.RandomInvert(),
-    transforms.RandomRotation(15),
+    # transforms.GaussianBlur(3),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.RandomPerspective(),
+    # transforms.RandomInvert(),
+    # transforms.RandomRotation(15),
     transforms.ToTensor(),
     transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
 ])
@@ -88,7 +86,10 @@ print(summary(model,input_data=(3,IMG_WIDTH,IMG_HEIGHT)))
 criterion=CrossEntropyLoss()
 optimizer=Adam(params=model.parameters(),lr=LR)
 # schedular=StepLR(optimizer=optimizer,step_size=5)
-metric=Accuracy()
+acc_metric=Accuracy()
+prec_metric=Precision()
+recall_metric=Recall()
+f1_metric=F1Score()
 running_loss=RunningLoss()
 ckpt_callback=CheckpointCallback(os.path.join(CKPT_DIR,MODEL_NAME),'max',verbose=1)
 
@@ -96,8 +97,11 @@ wandb.watch(model,criterion=criterion,log_freq=1,log_graph=True)
 
 for e in range(EPOCHS):
     model.train()
-    metric.reset()
+    acc_metric.reset()
     running_loss.reset()
+    prec_metric.reset()
+    recall_metric.reset()
+    f1_metric.reset()
     log_dict={}
     iter_loop=tqdm(enumerate(train_loader),total=len(train_loader))
     # running_loss=0
@@ -112,15 +116,30 @@ for e in range(EPOCHS):
         optimizer.step()
         
         # running_loss+=loss.detach().cpu().item()
-        running_loss.update(loss)
-        metric.update(label_batch,output)
+        running_loss.update(batch_loss=loss)
+        acc_metric.update(y_true=label_batch,y_pred=output)
+        prec_metric.update(y_true=label_batch,y_pred=output)
+        recall_metric.update(y_true=label_batch,y_pred=output)
+        f1_metric.update(y_true=label_batch,y_pred=output)
         iter_loop.set_description('TRAIN LOOP E: '+str(e))
-        iter_loop.set_postfix({'LOSS':running_loss.get_value(),"ACC":metric.get_value()})
+        iter_loop.set_postfix({
+            running_loss.name:running_loss.get_value(),
+            acc_metric.name:acc_metric.get_value(),
+            prec_metric.name:prec_metric.get_value(),
+            recall_metric.name:recall_metric.get_value(),
+            f1_metric.name:f1_metric.get_value()
+            })
     log_dict['loss/train']=running_loss.get_value()
-    log_dict['accuracy/train']=metric.get_value()
+    log_dict['accuracy/train']=acc_metric.get_value()
+    log_dict['precision/train']=prec_metric.get_value()
+    log_dict['recall/train']=recall_metric.get_value()
+    log_dict['f1score/train']=f1_metric.get_value()
     model.eval()
-    metric.reset()
+    acc_metric.reset()
     running_loss.reset()
+    prec_metric.reset()
+    recall_metric.reset()
+    f1_metric.reset()
     with torch.no_grad():
         iter_loop=tqdm(enumerate(valid_loader),total=len(valid_loader))
         for ii,(img_batch,label_batch) in iter_loop:
@@ -131,15 +150,27 @@ for e in range(EPOCHS):
 
             
             # loss+=loss.detach().cpu().numpy()
-            running_loss.update(loss)
-            metric.update(label_batch,output)
+            running_loss.update(batch_loss=loss)
+            acc_metric.update(y_true=label_batch,y_pred=output)
+            prec_metric.update(y_true=label_batch,y_pred=output)
+            recall_metric.update(y_true=label_batch,y_pred=output)
+            f1_metric.update(y_true=label_batch,y_pred=output)
             iter_loop.set_description('VALID LOOP E: '+str(e))
-            iter_loop.set_postfix({'LOSS':running_loss.get_value(),"ACC":metric.get_value()})
-        ckpt_callback.check_and_save(model,metric.get_value())   
+            iter_loop.set_postfix({
+                running_loss.name:running_loss.get_value(),
+                acc_metric.name:acc_metric.get_value(),
+                prec_metric.name:prec_metric.get_value(),
+                recall_metric.name:recall_metric.get_value(),
+                f1_metric.name:f1_metric.get_value()
+            })
+        ckpt_callback.check_and_save(model,acc_metric.get_value())   
     # schedular.step()
                  
     log_dict['loss/valid']=running_loss.get_value()
-    log_dict['accuracy/valid']=metric.get_value()
+    log_dict['accuracy/valid']=acc_metric.get_value()
+    log_dict['precision/valid']=prec_metric.get_value()
+    log_dict['recall/valid']=recall_metric.get_value()
+    log_dict['f1score/valid']=f1_metric.get_value()
     log_dict['epochs']=e
     # log_dict['lr']=schedular.get_last_lr()[-1]
     wandb.log(log_dict)
