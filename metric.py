870c2455fed637146f38f28577a735715feade55
hazemahmed45 <hazemahmed45@gmail.com>
2021-11-19 00:28:10 +0200

    added precision, recall, and f1score and abstract class metric

--- metric.py
+++ metric.py
@@ -1,13 +1,29 @@
+from abc import abstractmethod
 import torch
 import numpy as np
-
-class Accuracy():
+from sklearn.metrics import recall_score,precision_score,f1_score
+class Metric():
     def __init__(self) -> None:
+        self.name='metric'
+    @abstractmethod
+    def update(self,**kwargs):
+        return 
+    @abstractmethod
+    def get_value(self):
+        return 
+    @abstractmethod
+    def reset(self):
+        return 
+class Accuracy(Metric):
+    def __init__(self):
+        self.name='Accuracy'
         self.value=0
         self.num=0
         return 
-    def update(self,targets:torch.Tensor,prediciton:torch.Tensor):
+    def update(self,**kwargs):
         self.num+=1
+        targets=kwargs['y_true']
+        prediciton=kwargs['y_pred']
         targets=targets.detach().cpu()
         prediciton=prediciton.detach().cpu()
         batch_size=targets.shape[0]
@@ -20,12 +36,14 @@ class Accuracy():
         self.value=0
         self.num=0
         
-class RunningLoss():
-    def __init__(self) -> None:
+class RunningLoss(Metric):
+    def __init__(self):
+        self.name='Loss'
         self.value=0
         self.num=0
-    def update(self,step_loss:torch.Tensor):
+    def update(self,**kwargs):
         self.num+=1
+        step_loss=kwargs['batch_loss']
         self.value+=step_loss.detach().cpu().item()
         return 
     def get_value(self):
@@ -34,13 +52,70 @@ class RunningLoss():
         self.value=0
         self.num=0
         return 
+   
     
-class Precision():
-    def __init__(self) -> None:
-        pass
-class Recall():
-    def __init__(self) -> None:
-        pass
-class F1Score():
-    def __init__(self) -> None:
-        pass
+class Precision(Metric):
+    def __init__(self):
+        self.name='Precision'
+        # self.tp=0
+        # self.fp=0
+        self.value=0
+        self.num=0
+        return 
+    def update(self,**kwargs):
+        self.num+=1
+        y_true=kwargs['y_true']
+        y_pred=kwargs['y_pred']
+        y_true=y_true.detach().cpu().numpy()
+        y_pred=y_pred.detach().cpu().numpy().argmax(axis=1)
+        
+        # print(y_true)
+        # print(y_pred)
+        self.value+=precision_score(y_true,y_pred,average='macro',zero_division=0)
+        # print(precision_score(y_true,y_pred,average='macro'))
+        # exit()
+        return 
+    def reset(self):
+        self.num=0
+        self.value=0
+        return 
+    def get_value(self):
+        return self.value/self.num
+class Recall(Metric):
+    def __init__(self):
+        self.name='Recall'
+        self.value=0
+        self.num=0
+        return 
+    def update(self, **kwargs):
+        self.num+=1
+        y_true=kwargs['y_true']
+        y_pred=kwargs['y_pred']
+        y_true=y_true.detach().cpu().numpy()
+        y_pred=y_pred.detach().cpu().numpy().argmax(axis=1)
+        self.value+=recall_score(y_true,y_pred,average='macro',zero_division=0)
+        return 
+    def reset(self):
+        self.num=0
+        self.value=0
+        return 
+    def get_value(self):
+        return self.value/self.num
+class F1Score(Metric):
+    def __init__(self):
+        self.name='F1Score'
+        self.recall=Recall()
+        self.precision=Precision()
+        return 
+    def update(self, **kwargs):
+        y_true=kwargs['y_true']
+        y_pred=kwargs['y_pred']
+        self.recall.update(y_true=y_true,y_pred=y_pred)
+        self.precision.update(y_true=y_true,y_pred=y_pred)
+        return
+    def reset(self):
+        self.precision.reset()
+        self.recall.reset()
+        return
+    def get_value(self):
+        return 2*((self.precision.get_value()*self.recall.get_value())/(self.precision.get_value()+self.recall.get_value()))
